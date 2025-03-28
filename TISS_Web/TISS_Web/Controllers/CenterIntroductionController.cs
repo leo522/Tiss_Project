f9439dd73c0f588557bd480816ee1c663d93ece4
Leo <tou522@yahoo.com.tw>
2025-03-29 07:49:43 +0800

    優化 中心介紹控制器方法

--- TISS_Web/TISS_Web/Controllers/CenterIntroductionController.cs
+++ TISS_Web/TISS_Web/Controllers/CenterIntroductionController.cs
@@ -4,334 +4,95 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using TISS_Web.Models;
+using TISS_Web.Utility;
 
 namespace TISS_Web.Controllers
 {
     public class CenterIntroductionController : Controller
     {
         private TISS_WebEntities _db = new TISS_WebEntities(); //資料庫
-
-        #region 網頁內容存檔共用服務
-        private readonly WebContentService _contentService;
-        #endregion
-
-        #region 檔案上傳共用服務
-        private readonly FileUploadService _fileUploadService;
+        private readonly WebContentService _contentService; //網頁內容存檔共用服務
+        private readonly FileUploadService _fileUploadService; //檔案上傳共用服務
 
         public CenterIntroductionController()
         {
-            try
-            {
-                _fileUploadService = new FileUploadService(new TISS_WebEntities());
-                _contentService = new WebContentService(new TISS_WebEntities()); //網頁內容存檔共用服務
-            }
-            catch (Exception ex)
-            {
-
-                throw ex;
-            }
+            _contentService = new WebContentService(_db);
+            _fileUploadService = new FileUploadService(_db);
         }
-        #endregion
 
-        #region 中心介紹
-        public ActionResult about()
-        {
-            try
-            {
-                ViewBag.Title = "中心介紹";
-                Session["ReturnUrl"] = Request.Url.ToString();
-                return View();
-            }
-            catch (Exception ex)
-            {
-                throw ex;
-            }
-        }
-        
-        [HttpPost]
-        [ValidateInput(false)]
-        public ActionResult aboutSaveContent(string textContent, string ImageSrc)
+        #region 共用方法：儲存與讀取內容
+        private ActionResult SavePageContent<T>(string textContent, string imageSrc) where T : class, new()
         {
             try
             {
                 var userName = Session["UserName"] as string;
-                return _contentService.SaveContent(userName, textContent, ImageSrc, () => new AboutPageContent());
+                var result = _contentService.SaveContent(userName, textContent, imageSrc, () => new T());
+                return result; // 成功就不寫 Log
             }
             catch (Exception ex)
             {
+                LogHelper.WriteAduioVideoLog("SavePageContent", $"儲存 {typeof(T).Name} 錯誤", ex);
                 return Json(new { success = false, error = ex.Message });
             }
         }
-      
-        [HttpGet]
-        public ActionResult aboutGetContent()
+
+        private ActionResult GetPageContent<T>() where T : class
         {
             try
             {
-                return _contentService.GetContent<AboutPageContent>();
+                return _contentService.GetContent<T>();
             }
             catch (Exception ex)
             {
+                LogHelper.WriteAduioVideoLog("GetPageContent", $"讀取 {typeof(T).Name} 錯誤", ex);
                 return Json(new { success = false, error = ex.Message }, JsonRequestBehavior.AllowGet);
             }
         }
         #endregion
 
-        #region 使命願景 
-        public ActionResult Objectives()
-        {
-            try
-            {
-                ViewBag.Title = "使命、願景";
-                Session["ReturnUrl"] = Request.Url.ToString();
-                return View();
-            }
-            catch (Exception ex)
-            {
-                throw ex;
-            }
-        }
+        #region 各區塊頁面
+        public ActionResult about() => LoadView("中心介紹");
+        [HttpPost, ValidateInput(false)] public ActionResult AboutSaveContent(string textContent, string ImageSrc) => SavePageContent<AboutPageContent>(textContent, ImageSrc);
+        [HttpGet] public ActionResult AboutGetContent() => GetPageContent<AboutPageContent>();
 
-        [HttpPost]
-        [ValidateInput(false)]
-        public ActionResult objectivesSaveContent(string textContent, string ImageSrc)
-        {
-            try
-            {
-                var userName = Session["UserName"] as string;
-                return _contentService.SaveContent(userName, textContent, ImageSrc, () => new ObjectivesPageContent());
-            }
-            catch (Exception ex)
-            {
-                return Json(new { success = false, error = ex.Message });
-            }
-        }
-        [HttpGet]
-        public ActionResult objectivesGetContent()
-        {
-            try
-            {
-                return _contentService.GetContent<ObjectivesPageContent>();
-            }
-            catch (Exception ex)
-            {
-                return Json(new { success = false, error = ex.Message }, JsonRequestBehavior.AllowGet);
-            }
-        }
+        public ActionResult Objectives() => LoadView("使命、願景");
+        [HttpPost, ValidateInput(false)] public ActionResult ObjectivesSaveContent(string textContent, string ImageSrc) => SavePageContent<ObjectivesPageContent>(textContent, ImageSrc);
+        [HttpGet] public ActionResult ObjectivesGetContent() => GetPageContent<ObjectivesPageContent>();
+
+        public ActionResult Mission() => LoadView("中心任務");
+        [HttpPost, ValidateInput(false)] public ActionResult MissionSaveContent(string textContent, string ImageSrc) => SavePageContent<MissionPageContent>(textContent, ImageSrc);
+        [HttpGet] public ActionResult MissionGetContent() => GetPageContent<MissionPageContent>();
+
+        public ActionResult Organization() => LoadView("組織概況");
+        [HttpPost, ValidateInput(false)] public ActionResult OrganizationSaveContent(string textContent, string ImageSrc) => SavePageContent<OrganizationPageContent>(textContent, ImageSrc);
+        [HttpGet] public ActionResult OrganizationGetContent() => GetPageContent<OrganizationPageContent>();
+
+        public ActionResult BOD() => LoadView("第1屆 董監事");
+        [HttpPost, ValidateInput(false)] public ActionResult BODSaveContent(string textContent, string ImageSrc) => SavePageContent<BODPageContent>(textContent, ImageSrc);
+        [HttpGet] public ActionResult BODGetContent() => GetPageContent<BODPageContent>();
+
+        public ActionResult CEO() => LoadView("執行長");
+        [HttpPost, ValidateInput(false)] public ActionResult CEOSaveContent(string textContent, string ImageSrc) => SavePageContent<CEOPageContent>(textContent, ImageSrc);
+        [HttpGet] public ActionResult CEOGetContent() => GetPageContent<CEOPageContent>();
+
+        public ActionResult Units() => LoadView("單位介紹");
+        [HttpPost, ValidateInput(false)] public ActionResult UnitsSaveContent(string textContent, string ImageSrc) => SavePageContent<UnitsPageContent>(textContent, ImageSrc);
+        [HttpGet] public ActionResult UnitsGetContent() => GetPageContent<UnitsPageContent>();
         #endregion
 
-        #region 中心任務   
-        public ActionResult mission()
+        #region 共用 View 載入
+        private ActionResult LoadView(string title)
         {
             try
             {
-                ViewBag.Title = "中心任務";
+                ViewBag.Title = title;
                 Session["ReturnUrl"] = Request.Url.ToString();
                 return View();
             }
             catch (Exception ex)
             {
-                throw ex;
-            }
-        }
-
-        [HttpPost]
-        [ValidateInput(false)]
-        public ActionResult missionSaveContent(string textContent, string ImageSrc)
-        {
-            try
-            {
-                var userName = Session["UserName"] as string;
-                return _contentService.SaveContent(userName, textContent, ImageSrc, () => new MissionPageContent());
-            }
-            catch (Exception ex)
-            {
-                return Json(new { success = false, error = ex.Message });
-            }
-        }
-        [HttpGet]
-        public ActionResult missionGetContent()
-        {
-            try
-            {
-                return _contentService.GetContent<MissionPageContent>();
-            }
-            catch (Exception ex)
-            {
-                return Json(new { success = false, error = ex.Message }, JsonRequestBehavior.AllowGet);
-            }
-        }
-        #endregion
-
-        #region 組織概況
-        public ActionResult Organization()
-        {
-            try
-            {
-                ViewBag.Title = "組織概況";
-                Session["ReturnUrl"] = Request.Url.ToString();
-                return View();
-            }
-            catch (Exception ex)
-            {
-                throw ex;
-            }
-        }
-
-        [HttpPost]
-        [ValidateInput(false)]
-        public ActionResult organizationSaveContent(string textContent, string ImageSrc)
-        {
-            try
-            {
-                var userName = Session["UserName"] as string;
-                return _contentService.SaveContent(userName, textContent, ImageSrc, () => new OrganizationPageContent());
-            }
-            catch (Exception ex)
-            {
-                return Json(new { success = false, error = ex.Message });
-            }
-        }
-        [HttpGet]
-        public ActionResult organizationGetContent()
-        {
-            try
-            {
-                return _contentService.GetContent<OrganizationPageContent>();
-            }
-            catch (Exception ex)
-            {
-                return Json(new { success = false, error = ex.Message }, JsonRequestBehavior.AllowGet);
-            }
-        }
-        #endregion
-
-        #region 董監事
-        public ActionResult BOD()
-        {
-            try
-            {
-                ViewBag.Title = "第1屆 董監事";
-                Session["ReturnUrl"] = Request.Url.ToString();
-                return View();
-            }
-            catch (Exception ex)
-            {
-                throw ex;
-            }
-        }
-
-        [HttpPost]
-        [ValidateInput(false)]
-        public ActionResult BODSaveContent(string textContent, string ImageSrc)
-        {
-            try
-            {
-                var userName = Session["UserName"] as string;
-                return _contentService.SaveContent(userName, textContent, ImageSrc, () => new BODPageContent());
-            }
-            catch (Exception ex)
-            {
-                return Json(new { success = false, error = ex.Message });
-            }
-        }
-        [HttpGet]
-        public ActionResult BODGetContent()
-        {
-            try
-            {
-                return _contentService.GetContent<BODPageContent>();
-            }
-            catch (Exception ex)
-            {
-                return Json(new { success = false, error = ex.Message }, JsonRequestBehavior.AllowGet);
-            }
-        }
-        #endregion
-
-        #region 執行長
-        public ActionResult CEO()
-        {
-            try
-            {
-                ViewBag.Title = "執行長";
-                Session["ReturnUrl"] = Request.Url.ToString();
-                return View();
-            }
-            catch (Exception ex)
-            {
-                throw ex;
-            }
-        }
-
-        [HttpPost]
-        [ValidateInput(false)]
-        public ActionResult CeoSaveContent(string textContent, string ImageSrc)
-        {
-            try
-            {
-                var userName = Session["UserName"] as string;
-                return _contentService.SaveContent(userName, textContent, ImageSrc, () => new CEOPageContent());
-            }
-            catch (Exception ex)
-            {
-                return Json(new { success = false, error = ex.Message });
-            }
-        }
-        [HttpGet]
-        public ActionResult CeoGetContent()
-        {
-            try
-            {
-                return _contentService.GetContent<CEOPageContent>();
-            }
-            catch (Exception ex)
-            {
-                return Json(new { success = false, error = ex.Message }, JsonRequestBehavior.AllowGet);
-            }
-        }
-        #endregion
-
-        #region 單位介紹
-        public ActionResult Units()
-        {
-            try
-            {
-                ViewBag.Title = "單位介紹";
-                Session["ReturnUrl"] = Request.Url.ToString();
-                return View();
-            }
-            catch (Exception ex)
-            {
-                throw ex;
-            }
-        }
-
-        [HttpPost]
-        [ValidateInput(false)]
-        public ActionResult unitsSaveContent(string textContent, string ImageSrc)
-        {
-            try
-            {
-                var userName = Session["UserName"] as string;
-                return _contentService.SaveContent(userName, textContent, ImageSrc, () => new UnitsPageContent());
-            }
-            catch (Exception ex)
-            {
-                return Json(new { success = false, error = ex.Message });
-            }
-        }
-
-        [HttpGet]
-        public ActionResult unitsGetContent()
-        {
-            try
-            {
-                return _contentService.GetContent<UnitsPageContent>();
-            }
-            catch (Exception ex)
-            {
-                return Json(new { success = false, error = ex.Message }, JsonRequestBehavior.AllowGet);
+                LogHelper.WriteAduioVideoLog("LoadView", $"載入 {title} 頁面錯誤", ex);
+                return View("Error");
             }
         }
         #endregion
